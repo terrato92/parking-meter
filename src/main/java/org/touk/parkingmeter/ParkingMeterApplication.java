954852b0dd3f791b003392a53252660fa4cc8c34
terrato92 <kamilmituraa@gmail.com>
2018-05-22 01:47:59 +0200

    first commit

---
+++ src/main/java/org/touk/parkingmeter/ParkingMeterApplication.java
@@ -0,0 +1,12 @@
+package org.touk.parkingmeter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingMeterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingMeterApplication.class, args);
+	}
+}
