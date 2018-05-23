2605c27e90a8dad12b0b6fa914f7923e3e7135a0
terrato92 <kamilmituraa@gmail.com>
2018-05-23 05:12:25 +0200

    test parkingMachine commit

--- src/test/java/org/touk/parkingmeter/ParkingMachineServiceApplicationTests.java
+++ src/test/java/org/touk/parkingmeter/ParkingMachineServiceApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ParkingMachineApplicationTests {
+public class ParkingMachineServiceApplicationTests {
 
 	@Test
 	public void contextLoads() {
