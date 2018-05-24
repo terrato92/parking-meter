79d9e0de5396e331bfe012d3202e1d87edf8f0ba
terrato92 <kamilmituraa@gmail.com>
2018-05-24 18:58:04 +0200

    service commit

--- src/main/java/org/touk/parkingmeter/service/implementation/ICounterServiceVip.java
+++ src/main/java/org/touk/parkingmeter/service/implementation/ICounterServiceVip.java
@@ -1,13 +1,36 @@
 package org.touk.parkingmeter.service.implementation;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.touk.parkingmeter.domain.User;
+import org.touk.parkingmeter.repositories.UserRepository;
 import org.touk.parkingmeter.service.CounterService;
 
 public class ICounterServiceVip implements CounterService {
 
+    @Autowired
+    private final UserRepository userRepository;
+
+    public ICounterServiceVip(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
     public double parkingRates(User user, Long timeAtParking) {
 
-        return 0;
+        long diffHours = timeAtParking / (60 * 60 * 1000) % 24;
+        double fee = 0;
+
+        if (diffHours <= 1) {
+            fee = 0;
+        } else if (1 < diffHours && diffHours <= 2) {
+            fee= 1;
+        } else if (2 < diffHours) {
+
+            for (int i = 2; i < diffHours; i++) {
+                fee = i * 1.2;
+            }
+        }
+        return fee;
     }
+
 }
