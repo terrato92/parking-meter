10c133d29a371bfe3bd36d80bd4cf2f6dd33bc6d
terrato92 <kamilmituraa@gmail.com>
2018-05-26 17:32:36 +0200

    test commit

---
+++ src/test/java/org/touk/parkingmeter/controller/ParkingControllerTest.java
@@ -0,0 +1,25 @@
+package org.touk.parkingmeter.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ParkingControllerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void addTicket() {
+    }
+
+    @Test
+    public void closeTicket() {
+    }
+
+    @Test
+    public void checkFee() {
+    }
+}
