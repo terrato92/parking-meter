159de27c52e54227063e03f8e4ad3fa33271c9d7
terrato92 <kamilmituraa@gmail.com>
2018-05-28 00:24:51 +0200

    repo commit

--- src/main/java/org/touk/parkingmeter/repositories/TicketRepository.java
+++ src/main/java/org/touk/parkingmeter/repositories/TicketRepository.java
@@ -5,6 +5,9 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.touk.parkingmeter.domain.Ticket;
 
+import java.util.Optional;
+
 @Repository
 public interface TicketRepository extends CrudRepository<Ticket, Long> {
+
 }
