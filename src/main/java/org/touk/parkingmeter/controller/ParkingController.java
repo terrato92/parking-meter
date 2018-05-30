41c9463b82de693def95941a34b943fe6246bc9f
terrato92 <kamilmituraa@gmail.com>
2018-05-30 03:14:49 +0200

    controller commit

--- src/main/java/org/touk/parkingmeter/controller/ParkingController.java
+++ src/main/java/org/touk/parkingmeter/controller/ParkingController.java
@@ -5,36 +5,41 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.touk.parkingmeter.converter.ConvertToFeeDto;
+import org.touk.parkingmeter.domain.Currency;
 import org.touk.parkingmeter.domain.Ticket;
+import org.touk.parkingmeter.dto.DataDto;
+import org.touk.parkingmeter.dto.FeeDto;
 import org.touk.parkingmeter.service.ParkingMachineService;
 import org.touk.parkingmeter.service.UserService;
 
+import java.math.BigDecimal;
+
 @RestController
 public class ParkingController {
-
     private final ParkingMachineService parkingMachineService;
     private final UserService userService;
+    private final ConvertToFeeDto convertToFeeDto;
 
     @Autowired
     public ParkingController(
             ParkingMachineService parkingMachineService,
-            UserService userService) {
+            UserService userService, ConvertToFeeDto convertToFeeDto) {
 
         this.parkingMachineService = parkingMachineService;
         this.userService = userService;
+        this.convertToFeeDto = convertToFeeDto;
     }
 
 
-    @PostMapping("parking/{longitude}/{latitude}/user/{userId}/plate/{plate}")
-    public ResponseEntity<Ticket> addTicket(@PathVariable Long longitude,
-                                            @PathVariable Long latitude,
-                                            @PathVariable Long userId,
-                                            @PathVariable String plate) {
-
-        Ticket ticket = parkingMachineService.createTicket(longitude, latitude, userId, plate);
+    @PostMapping("/parking")
+    public ResponseEntity<Ticket> addTicket(@RequestBody DataDto data){
+        Ticket ticket = parkingMachineService.createTicket(data);
 
         return new ResponseEntity<Ticket>(ticket, HttpStatus.CREATED);
     }
+
+
 
     @PutMapping("ticket/{ticketId}")
     public ResponseEntity<Ticket> closeTicket(@PathVariable Long ticketId) {
@@ -45,10 +50,12 @@ public class ParkingController {
     }
 
     @GetMapping("ticket/{ticketId}")
-    public ResponseEntity<Double> checkFee(@PathVariable Long ticketId) {
+    public ResponseEntity<FeeDto> checkFee(@PathVariable Long ticketId) {
 
-        Double fee = userService.checkFee(ticketId);
+        BigDecimal fee = userService.checkFee(ticketId);
 
-        return new ResponseEntity<>(fee, HttpStatus.OK);
+        FeeDto feeDto = convertToFeeDto.convertToFeeDto(Currency.PLN, fee);
+
+        return new ResponseEntity<>(feeDto, HttpStatus.OK);
     }
 }
