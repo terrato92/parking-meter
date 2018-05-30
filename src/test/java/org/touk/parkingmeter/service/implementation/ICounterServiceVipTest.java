63b2169157ab881d48cc0e0bf5de597974abb864
terrato92 <kamilmituraa@gmail.com>
2018-05-30 16:24:39 +0200

    test commit

---
+++ src/test/java/org/touk/parkingmeter/service/implementation/ICounterServiceVipTest.java
@@ -0,0 +1,34 @@
+package org.touk.parkingmeter.service.implementation;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.touk.parkingmeter.service.CounterService;
+
+import java.math.BigDecimal;
+
+import static org.junit.Assert.*;
+
+public class ICounterServiceVipTest {
+
+    @Mock
+    private CounterService counterService;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        counterService = new ICounterServiceVip();
+    }
+
+    @Test
+    public void parkingRates() throws Exception {
+        Long time = 7524000L;
+
+        BigDecimal fee = counterService.parkingRates(time);
+
+        assertEquals(0, Double.compare(fee.doubleValue(), 4.4));
+    }
+
+}
